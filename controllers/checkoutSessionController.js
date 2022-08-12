75036c0975bc9db66dd2cde920f4a08a43f7495e
Ahmed Ismail <ahmedmhmd76@gmail.com>
2022-08-12 23:46:45 +0200

    fixing incorrect total amount issue

--- controllers/checkoutSessionController.js
+++ controllers/checkoutSessionController.js
@@ -4,7 +4,7 @@ let User = mongoose.model('users');
 require('../models/productModel');
 let Products = mongoose.model('products');
 // This is your test secret API key.
-const stripe = require('stripe')(process.env.stripe_secret_key);
+const stripe = require('stripe')('sk_test_51LQaYPH7xAqsB8WshWlLzJ5yHz1vsVFWXZg4M8HimY34jqeulGehRY7ghrZM8IEHywVQIsaSGokP7VPfZgyDy3G000fCheaDX6');
 
 const YOUR_DOMAIN = 'http://localhost:3000';
 
@@ -21,7 +21,7 @@ module.exports.createCheckoutSession = async (request, response, next) => {
                 {
                     name: `${productData.name}`,
                     description: productData.description,
-                    amount: ((productData.price - productData.discount) * cartData.quantity).toFixed(2) * 100,
+                    amount: (productData.price - productData.discount).toFixed(2) * 100,
                     quantity: cartData.quantity,
                     currency: 'egp',
                 },
